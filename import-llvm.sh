44d748a87385d0d6bde7ccffe6259277ad1750bf
Lauri Vasama <vasama@rev.ng>
2025-01-31 12:28:06 +0200

    WIP: Add llvm-import scripts

---
+++ import-llvm.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+SAMPLE_PATH="/home/vasama/c/revng/orchestra/sources/revng/examples"
+SAMPLE_IR_PATH="$SAMPLE_PATH/$1.ll"
+SAMPLE_MODEL_PATH="$SAMPLE_PATH/model.yml"
+
+bin/revng clift-opt \
+	--import-llvm="llvm=$SAMPLE_IR_PATH model=$SAMPLE_MODEL_PATH" \
+	/dev/null
