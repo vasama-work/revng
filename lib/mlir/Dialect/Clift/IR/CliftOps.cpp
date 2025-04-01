1e8c66f2fff3d0afad577ea3a6a49ea3abb7344e
Lauri Vasama <vasama@rev.ng>
2025-04-01 09:16:09 +0300

    Add Clift label op helpers

--- lib/mlir/Dialect/Clift/IR/CliftOps.cpp
+++ lib/mlir/Dialect/Clift/IR/CliftOps.cpp
@@ -703,6 +703,10 @@ MakeLabelOp GoToOp::getLabelOp() {
   return getLabel().getDefiningOp<MakeLabelOp>();
 }
 
+AssignLabelOp GoToOp::getAssignLabelOp() {
+  return getLabelOp().getAssignLabelOp();
+}
+
 //===-------------------------------- IfOp --------------------------------===//
 
 mlir::LogicalResult IfOp::verify() {
@@ -776,6 +780,14 @@ mlir::LogicalResult MakeLabelOp::verify() {
                          << " must have an assignment.";
 
   return mlir::success();
+}
+
+AssignLabelOp MakeLabelOp::getAssignLabelOp() {
+  for (mlir::Operation *User : getOperation()->getUsers()) {
+    if (auto AssignLabel = mlir::dyn_cast<AssignLabelOp>(User))
+      return AssignLabel;
+  }
+  return {};
 }
 
 //===------------------------------ ReturnOp ------------------------------===//
