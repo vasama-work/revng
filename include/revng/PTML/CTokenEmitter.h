2195ddceb298a7f64ea594252077d6d9c6a3a5a3
Lauri Vasama <vasama@rev.ng>
2025-12-16 07:03:41 +0200

    Emit PTML instruction tags from Clift

--- include/revng/PTML/CTokenEmitter.h
+++ include/revng/PTML/CTokenEmitter.h
@@ -248,7 +248,7 @@ public:
                    ScopeKind Kind,
                    Delimiter Delimiter,
                    int Indent) :
-      Emitter(Emitter), Delimiter(Delimiter), Indent(Indent), Tag() {
+      Emitter(Emitter), Delimiter(Delimiter), Indent(Indent) {
       Emitter.enterScopeImpl(Tag, Delimiter, Indent, Kind);
     }
 
@@ -271,6 +271,28 @@ public:
     return Scope(*this, Kind, Delimiter, Indent);
   }
 
+  enum class RegionKind : uint8_t {
+    Expression,
+  };
+
+  class Region {
+    ptml::Emitter::TagEmitter Tag;
+
+  public:
+    explicit Region(CTokenEmitter &Emitter,
+                    RegionKind Kind,
+                    llvm::StringRef Location) {
+      Emitter.enterRegionImpl(Tag, Kind, Location);
+    }
+
+    Region(const Region &) = delete;
+    Region &operator=(const Region &) = delete;
+  };
+
+  [[nodiscard]] Region enterRegion(RegionKind Kind, llvm::StringRef Location) {
+    return Region(*this, Kind, Location);
+  }
+
 private:
   void enterCommentImpl(CommentEmitter &Comment);
   void leaveCommentImpl(CommentEmitter &Comment);
@@ -288,4 +310,8 @@ private:
   void leaveScopeImpl(ptml::Emitter::TagEmitter &Tag,
                       Delimiter Delimiter,
                       int Indent);
+
+  void enterRegionImpl(ptml::Emitter::TagEmitter &Tag,
+                       RegionKind Kind,
+                       llvm::StringRef Location);
 };
